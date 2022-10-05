86a16f94a0bd5c18c8d68d6df5faee1ca6a82a88
iskinf <89261665+iskinf@users.noreply.github.com>
2022-10-05 22:22:23 +0200

    Add files via upload

---
+++ Coursera Tools for Data Science.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## "My Jupyter Notebook on IBM Watson Studio"
+
+# ### **Faruk Iskin** 
+# ### **Geotechnical Engineer**
+
+# ### *I have an interest for the data science to automatize many challenging tasks in my field, which have been dealt with by means of obsolete methods.*
+
+# #### The following returns the sum of 1 and 2
+
+# In[9]:
+
+
+1+2
+
+
+# >*Links to challenging projects:*
+# 
+# <ol>
+# 
+# <li>In TR</li> 
+#     
+# __[Eurasia Tunnel](https://www.aa.com.tr/uploads/TempUserFiles/eurasia-tunnel.jpg)__
+#     
+# <li>In AT</li> 
+#     
+# __[Brenner Base Tunnel](https://cdn.ca.emap.com/wp-content/uploads/sites/9/2021/05/21-04-28_Herrenknecht-Press-Release_Brenner-Base-Tunnel_Photo-1-1024x683.jpg)__
+#     
+# <li>In GB</li> 
+#     
+# __[HS2](https://cdn.gillespies.co.uk/uploads/projects/_ogImage/HS2-Engineering-Support-Arup-train-2000x1125.jpg?v=1628090843)__  
+# >*Thank you for your time*   
+
+# In[ ]:
+
+
+
+
